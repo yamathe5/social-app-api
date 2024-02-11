50f7e4aa1e4f8a187324bf9f869e45eefab8759e
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-11 17:56:35 -0500

    ADD: comment Routes commentRoutes.js

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const commentController = require('../controllers/commentController');
 const router = express.Router();
 
-router.get('/comments', (req, res) => {
-  res.send('Lista de comments');
-});
+// Obtener todos los comentarios de una publicación
+router.get('/posts/:postId/comments', commentController.getCommentsByPostId);
 
-router.post('/comments', (req, res) => {
-  res.send('Cometario creado');
-});
+// Añadir un nuevo comentario a una publicación
+router.post('/posts/:postId/comments', commentController.addCommentToPost);
+
+// Actualizar un comentario específico
+router.put('/comments/:commentId', commentController.updateComment);
+
+// Eliminar un comentario específico
+router.delete('/comments/:commentId', commentController.deleteComment);
+
+// Rutas adicionales relacionadas con los comentarios, si las necesitas
 
 module.exports = router;
