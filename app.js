5904fafcf33dbd542c6e703a59b16087b4566f18
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-11 17:53:57 -0500

    ADD: appjs & routes

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const postRoutes = require('./routes/postRoutes'); // Asegúrate de que la ruta 
 const commentRoutes = require('./routes/commentRoutes'); // Asegúrate de que la ruta sea correcta
 const app = express();
 const port = 3000;
+const connectDB = require('./config/db'); // Asegúrate de que la ruta sea correcta
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 // Ruta base
 app.get('/', (req, res) => {
