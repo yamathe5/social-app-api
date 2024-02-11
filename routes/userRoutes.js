a3d5cbdd3efb5169c46c4996ffa1b3e846a74d31
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-11 17:57:00 -0500

    ADD: user routes in usersRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const {userController} = require('../controllers/userController');
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-  res.send('Lista de usuarios');
-});
+// Registrarse como nuevo usuario
+router.post('/signup', userController.userSignup);
 
-router.post('/users', (req, res) => {
-  res.send('Usuario creado');
-});
+// Iniciar sesión
+router.post('/login', userController.userLogin);
+
+// Ver perfil de usuario por ID
+router.get('/user/:userId', userController.getUserProfile);
+
+// Actualizar perfil de usuario
+router.put('/user/:userId', userController.updateUserProfile);
+
+// Eliminar un usuario
+router.delete('/user/:userId', userController.deleteUser);
+
+// Seguir a un usuario
+router.post('/user/:userId/follow', userController.followUser);
+
+// Dejar de seguir a un usuario
+router.delete('/user/:userId/follow', userController.unfollowUser);
+
+// Obtener la lista de seguidores de un usuario
+router.get('/user/:userId/followers', userController.getUserFollowers);
+
+// Obtener la lista de usuarios seguidos por un usuario
+router.get('/user/:userId/following', userController.getUserFollowing);
+
+// Rutas adicionales relacionadas con usuarios, si las necesitas
 
 module.exports = router;
