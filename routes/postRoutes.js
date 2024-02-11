e9e16678b74f74644cc81ecaba2a02d6b041bd0d
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-11 17:56:46 -0500

    ADD: post Routes postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const postController = require('../controllers/postController');
 const router = express.Router();
 
-router.get('/posts', (req, res) => {
-  res.send('Lista de posts');
-});
 
-router.post('/posts', (req, res) => {
-  res.send('Post creado');
-});
+
+// Obtener todos los posts
+router.get('/posts', postController.getAllPosts);
+
+// Obtener un post específico por su ID
+router.get('/posts/:id', postController.getPostById);
+
+// Obtener todos los posts de un usuario específico
+router.get('/user/:userId/posts', postController.getPostsByUserId);
+
+// Obtener un post específico de un usuario
+router.get('/user/:userId/posts/:postId', postController.getPostByUserId);
+
+// Crear un nuevo post
+router.post('/posts', postController.createPost);
+
+// Actualizar un post por su ID
+router.put('/posts/:id', postController.updatePostById);
+
+// Borrar un post por su ID
+router.delete('/posts/:id', postController.deletePostById);
+
+
+
 
 module.exports = router;
