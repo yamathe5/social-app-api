0db03ac6a4388da6eb7188662ba4f78dbd2c3384
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-12 18:22:52 -0500

    ADD: Middleware validation for userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const userController = require('../controllers/userController');
+const express = require("express");
+const userController = require("../controllers/userController");
 const router = express.Router();
-
+const { validarToken } = require("../middleware/webtokenvalidation");
+//
 // Registrarse como nuevo usuario
-router.post('/signup', userController.userSignup);
+router.post("/signup", userController.userSignup);
 
 // Iniciar sesión
-router.post('/login', userController.userLogin);
+router.post("/login", userController.userLogin);
 
 // Ver perfil de usuario por ID
-router.get('/user/:userId', userController.getUserProfile);
+router.get("/user/:userId", validarToken, userController.getUserProfile);
 
 // Actualizar perfil de usuario
-router.put('/user/:userId', userController.updateUserProfile);
+
+router.patch("/user/:userId",validarToken, userController.updateUserProfile);
 
 // Eliminar un usuario
-router.delete('/user/:userId', userController.deleteUser);
+router.delete("/user/:userId",validarToken, userController.deleteUser);
 
 // Seguir a un usuario
-router.post('/user/:userId/follow', userController.followUser);
+router.post("/user/:userId/follow", userController.followUser);
 
 // Dejar de seguir a un usuario
-router.delete('/user/:userId/follow', userController.unfollowUser);
+router.delete("/user/:userId/follow", userController.unfollowUser);
 
 // Obtener la lista de seguidores de un usuario
-router.get('/user/:userId/followers', userController.getUserFollowers);
+router.get("/user/:userId/followers", userController.getUserFollowers);
 
 // Obtener la lista de usuarios seguidos por un usuario
-router.get('/user/:userId/following', userController.getUserFollowing);
+router.get("/user/:userId/following", userController.getUserFollowing);
 
 // Rutas adicionales relacionadas con usuarios, si las necesitas
 
