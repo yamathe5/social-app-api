5af1429dcf959be3a4ece4ad97ead7f0be0b3e97
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-12 18:21:40 -0500

    ADD: webtokenvalidation function

---
+++ middleware/webtokenvalidation.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+exports.validarToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // "Bearer TOKEN"
+
+  if (!token) {
+    return res.status(401).json({ message: 'Acceso denegado' });
+  }
+
+  try {
+    // Verificar el token
+    const verificado = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verificado; 
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Token inválido' });
+  }
+};
+
