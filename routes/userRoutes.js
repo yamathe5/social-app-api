b962aa508b0c7b6ec1c1f8b5d78b158707c90c2b
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-18 14:13:14 -0500

    DELETE: follow routes in userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,17 +19,7 @@ router.patch("/user/:userId",validarToken, userController.updateUserProfile);
 // Eliminar un usuario
 router.delete("/user/:userId",validarToken, userController.deleteUser);
 
-// Seguir a un usuario
-router.post("/user/:userId/follow", userController.followUser);
 
-// Dejar de seguir a un usuario
-router.delete("/user/:userId/follow", userController.unfollowUser);
-
-// Obtener la lista de seguidores de un usuario
-router.get("/user/:userId/followers", userController.getUserFollowers);
-
-// Obtener la lista de usuarios seguidos por un usuario
-router.get("/user/:userId/following", userController.getUserFollowing);
 
 // Rutas adicionales relacionadas con usuarios, si las necesitas
 
