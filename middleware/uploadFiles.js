4956a3d1cc3921c5b06c33b56959ec3883f834c2
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-02-19 23:03:51 -0500

    ADD: image upload middleware using Multer and Backblaze B2 storage

---
+++ middleware/uploadFiles.js
@@ -0,0 +1,37 @@
+const multer = require("multer");
+const dotenv = require("dotenv");
+const B2 = require("backblaze-b2");
+
+dotenv.config();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+const uploadB2 = async (req, res, next) => {
+  const b2 = new B2({
+    applicationKeyId: process.env.KEY_ID,
+    applicationKey: process.env.APP_KEY,
+  });
+
+  const authResponse = await b2.authorize();
+  const { downloadUrl } = authResponse.data;
+
+  const response = await b2.getUploadUrl({ bucketId: process.env.BUCKET_ID });
+  const { authorizationToken, uploadUrl } = response.data;
+
+  const params = {
+    uploadUrl,
+    uploadAuthToken: authorizationToken,
+    fileName: `cod3a/${req.files[0].originalname}`,
+    data: req.files[0].buffer,
+  };
+
+  const fileInfo = await b2.uploadFile(params);
+
+  const url = `${downloadUrl}/file/${process.env.BUCKET_NAME}/${fileInfo.data.fileName}`;
+  res.locals.url = url;
+
+  next();
+};
+
+module.exports = { upload, uploadB2 };
